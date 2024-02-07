58830890d3c9d7371b7add1f6e923144b12425be
Aditya <wildflame365@gmail.com>
2024-02-07 21:39:50 +0530

    Added datasets

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,4 +2,24 @@
     import "../app.css";
 </script>
 
+<header class="flex flex-wrap p-2 gap-4">
+    <img class="h-20 w-20" src="https://www.ibm.com/brand/experience-guides/developer/8f4e3cc2b5d52354a6d43c8edba1e3c9/02_8-bar-reverse.svg" alt="IBM">
+    <div class="p-1">
+        <h1 class="text-3xl font-serif font-extrabold py-1 px-2">Major Project</h1>
+        <h2 class="text-xl font-serif font-bold text-purple-600 shadow-inner shadow-purple-700 px-2">Customer Behavior Analysis</h2>
+    </div>
+</header>
 <slot />
+<footer class="bg-purple-700 shadow-inner shadow-purple-800 h-48 p-4 w-full flex flex-wrap justify-between">
+    <div>
+        <p class="text-white font-serif font-bold text-xl p-1">Created by :</p>
+        <p class="text-white font-serif px-1">Aditya Narayan Panda</p>
+        <p class="text-white font-serif px-1">M.S.L.V. Saranya</p>
+        <p class="text-white font-serif px-1">Sonali Nanda</p>
+        <p class="text-white font-serif px-1">Bhandari Sree Shivani</p>
+    </div>
+    <div class="py-9">
+        <p class="text-white font-serif font-bold text-xl p-1 text-right">Guided by :</p>
+        <p class="text-white font-serif px-1">Dr. Chinmaya Kumar Nayak</p>
+    </div>
+</footer>
